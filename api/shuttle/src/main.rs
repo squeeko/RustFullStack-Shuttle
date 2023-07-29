17729613737705dee0aa50047cf3619328cc3d56
squeeko <squeeko@users.noreply.github.com>
2023-07-28 19:22:59 -0700

    setup database

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
+use sqlx::Executor;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -8,8 +10,12 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn actix_web(
-    #[shuttle_shared_db::Postgres()] _pool: sqlx::PgPool,
+    #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    // initialize the database if not already initialized
+    pool.execute(include_str!("../../db/schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
