1e436c3d01a405d34d00f7f418b6991d45b9fe0d
squeeko <squeeko@users.noreply.github.com>
2023-07-31 15:32:17 -0700

    ad version endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -8,6 +8,23 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+// #[get("/version")]
+// async fn version() -> &'static str {
+//     "version = 0.0.0"
+// }
+
+#[get("/version")]
+async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(db.get_ref())
+        .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
+
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
@@ -16,8 +33,11 @@ async fn actix_web(
     pool.execute(include_str!("../../db/schema.sql"))
         .await
         .map_err(CustomError::new)?;
+
+    let pool = actix_web::web::Data::new(pool);
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        // cfg.service(hello_world).service(version);
+        cfg.app_data(pool).service(hello_world).service(version);
     };
 
     Ok(config.into())
